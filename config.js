9518d611f4e3b844c054f91a3d524907bc7c3050
r1chgang <olo.kha@mail.ru>
2018-02-03 21:14:28 +0300

    Commit Summary

---
+++ config.js
@@ -0,0 +1,1 @@
+var youtube={ID:'ZQRsDx11pCg',start:13,end:0,repeat:true,mute:false};$('#background-effect').tubular({videoId:youtube.ID+'?controls=0&end='+ youtube.end+'&rel=0&version=3',repeat:youtube.repeat,start:youtube.start,mute:youtube.mute});
